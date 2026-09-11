76dc06ba342dcbbc89420361f9885c61a4f0f4cf
NadolnyJoao <nadolnyjoaovitor@gmail.com>
2026-09-11 16:44:07 -0300

    Atividades da aula 1, dia 24 de agosto de 2026

---
+++ Aula01.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    //Atividade 1
+    /*
+    double volume, nUsr;
+    scanf("%lf", &nUsr);
+    volume = (3.0/4.0) * M_PI * pow(nUsr, 3);
+    printf("%lf", volume);
+*/
+    //Atividade 2
+    /*
+    int nUsr1, nUsr2, nUsr3, nUsr4, nUsr5;
+    float media;
+    scanf("%d", &nUsr1);
+    scanf("%d", &nUsr2);
+    scanf("%d", &nUsr3);
+    scanf("%d", &nUsr4);
+    scanf("%d", &nUsr5);
+    media = (nUsr1 + nUsr2) / 2.0;
+    printf("%.1f\n", media);
+    media = (nUsr1 + nUsr2 + nUsr3) / 3.0;
+    printf("%.1f\n", media);
+    media = (nUsr1 + nUsr2 + nUsr3 + nUsr4) / 4.0;
+    printf("%.1f\n", media);
+    media = (nUsr1 + nUsr2 + nUsr3 + nUsr4 + nUsr5) / 5.0;
+    printf("%.1f\n", media);*/
+
+    //Atividade 3
+    /*
+    int segundos;
+    int dias, horas, minutos;
+
+    scanf("%d", &segundos);
+
+    dias = segundos / 86400;
+    segundos = segundos % 86400;
+
+    horas = segundos / 3600;
+    segundos = segundos % 3600;
+
+    minutos = segundos / 60;
+    segundos = segundos % 60;
+
+    printf("dias: %d\n", dias);
+    printf("horas: %d\n", horas);
+    printf("minutos: %d\n", minutos);
+    printf("segundos: %d\n", segundos);
+    */
+return 0;}
