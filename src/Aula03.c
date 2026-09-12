66a8c911b0856382ac5b1a30ac2a0eb556933228
NadolnyJoao <nadolnyjoaovitor@gmail.com>
2026-09-12 20:09:30 -0300

    Primeira atividade da aula 03

---
+++ src/Aula03.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    //Atividade 1
+    /*
+    float gasolina, alcool, kmGasolina, kmAlcool;
+
+    printf("Informe o preco da gasolina e do alcool: ");
+    scanf("%f %f", &gasolina, &alcool);
+
+    printf("Informe os km/l da gasolina e do alcool: ");
+    scanf("%f %f", &kmGasolina, &kmAlcool);
+
+    gasolina = gasolina / kmGasolina;
+    alcool = alcool / kmAlcool;
+
+    if (alcool < gasolina) {
+        printf("Alcool compensa mais");
+    } else {
+        printf("Gasolina compensa mais");
+    }*/
+
+
+
+    return 0;
+}
