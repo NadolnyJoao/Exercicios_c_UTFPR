ebb8eda600f84e3760a6c95a4c309ec3836d4b85
NadolnyJoao <nadolnyjoaovitor@gmail.com>
2026-09-12 20:10:29 -0300

    Segunda atividade da aula 03

--- src/Aula03.c
+++ src/Aula03.c
@@ -21,6 +21,48 @@ int main()
         printf("Gasolina compensa mais");
     }*/
 
+#include <stdio.h>
+
+int main()
+{
+    //Atividade 1
+    /*
+    float gasolina, alcool, kmGasolina, kmAlcool;
+
+    printf("Informe o preco da gasolina e do alcool: ");
+    scanf("%f %f", &gasolina, &alcool);
+
+    printf("Informe os km/l da gasolina e do alcool: ");
+    scanf("%f %f", &kmGasolina, &kmAlcool);
+
+    gasolina = gasolina / kmGasolina;
+    alcool = alcool / kmAlcool;
+
+    if (alcool < gasolina) {
+        printf("Alcool compensa mais");
+    } else {
+        printf("Gasolina compensa mais");
+    }*/
+
+    //Atividade 2
+    /*
+    int nUsr, n1, n2, n3, invertido;
+
+    scanf("%d", &nUsr);
+
+    n1 = nUsr / 100;        // centena
+    n2 = (nUsr / 10) % 10; // dezena
+    n3 = nUsr % 10;        // unidade
+
+    invertido = n3 * 100 + n2 * 10 + n1;
+
+    printf("%d", invertido);
+    */
+
+
+
+    return 0;
+}
 
 
     return 0;
