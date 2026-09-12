971d09a1554650faddf18639438b6b87409d9cc4
NadolnyJoao <nadolnyjoaovitor@gmail.com>
2026-09-12 20:27:30 -0300

    Quinta atividade da aula 03

--- src/Aula03.c
+++ src/Aula03.c
@@ -70,6 +70,33 @@ int main()
     printf("O jogo durou %d hora(s)", duracao);
 */
 
+    //Atividade 5
+   /*    double x, y;
+
+    scanf("%lf %lf", &x, &y);
+
+    if (x == 0 && y == 0) {
+        printf("Origem");
+    }
+    else if (y == 0) {
+        printf("Eixo X");
+    }
+    else if (x == 0) {
+        printf("Eixo Y");
+    }
+    else if (x > 0 && y > 0) {
+        printf("Q1");
+    }
+    else if (x < 0 && y > 0) {
+        printf("Q2");
+    }
+    else if (x < 0 && y < 0) {
+        printf("Q3");
+    }
+    else {
+        printf("Q4");
+    }
+    */
 
 
     return 0;
