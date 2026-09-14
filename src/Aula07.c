514e064b6833cab3d9b73ac9f708af49781ec6fa
NadolnyJoao <nadolnyjoaovitor@gmail.com>
2026-09-14 10:38:17 -0300

    Lista de exercícios da aula 07 completa

--- src/Aula07.c
+++ src/Aula07.c
@@ -1,5 +1,45 @@
 #include<stdio.h>
 int main(){
+    //Lista Aula 07;
+    /*
+    bool intervao_true = true;
+    int nUsr;
+    while (intervao_true){
+        scanf("\n %d", &nUsr);
+        if (10 > nUsr || nUsr > 20){
+            intervao_true = false;
+        }else
+        printf("ECO: %d\n", nUsr);
+    }
+
+    float Foolano = 1.50, Barano = 1.10;
+    int i;
+    for(i = 0; Foolano > Barano; i++){
+        Foolano += 0.2;
+        Barano += 0.3;
+    }
+    printf("Vai demorar %d anos ate Barano ficar mais alto que Foolano", i);
+
+    int min, max, nUsr = 0, nUsr_min = 10, nUsr_max = 1, i, qntos_ints = 0;
+
+    for(i = 0; nUsr >= 0; i++){
+        scanf ("%d", &nUsr);
+        if(1 <= nUsr && nUsr <= 10){
+            if(nUsr >= nUsr_max){
+                nUsr_max = nUsr;
+            }
+            if(nUsr <= nUsr_min){
+                nUsr_min = nUsr;
+            }
+            qntos_ints++;
+        }
+
+    }
+    if (qntos_ints){
+    printf("o maior numero que respeite o intervalo de 1 a 10 foi %d, e o menor foi %d", nUsr_max, nUsr_min);
+    }else printf("Erro, nenhum numero que respeite o intervalo foi dado");
+     */
+
 
 return 0;
 }
