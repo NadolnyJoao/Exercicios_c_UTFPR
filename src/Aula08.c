45d640fa2c4b26f342b1078b13941ac39987f24b
NadolnyJoao <nadolnyjoaovitor@gmail.com>
2026-09-15 21:41:55 -0300

    Primeira Atividade da lista 8 de introd a Prog

---
+++ src/Aula08.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main ()
+{
+    //Lista Aula 08;
+    unsigned int base, expoente;
+    unsigned long long total = 1;
+
+    scanf ("%u", &base);
+    scanf ("%u", &expoente);
+
+    while (expoente > 0)
+    {
+        total *= base;
+        expoente--;
+    }
+
+    printf ("%llu\n", total);
+
+    return (0);
+}
