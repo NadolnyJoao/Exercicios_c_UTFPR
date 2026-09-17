02eb9407edf342b5d3a32068d4be6b0851ba641c
NadolnyJoao <nadolnyjoaovitor@gmail.com>
2026-09-17 13:45:10 -0300

    Exercicio 1 da aula 8

--- src/Aula08.c
+++ src/Aula08.c
@@ -3,6 +3,7 @@
 int main ()
 {
     //Lista Aula 08;
+    /*
     unsigned int base, expoente;
     unsigned long long total = 1;
 
@@ -16,6 +17,23 @@ int main ()
     }
 
     printf ("%llu\n", total);
+    */
+
+   //Exercicio 1;
+   int ai, i = 1;
+   scanf("%d", &ai);
+
+   while(ai > 1){
+
+    if(ai%2 == 0){
+        ai = ai / 2;
+    }else
+        ai = (ai * 3) + 1;
+
+    printf("%d, ", ai);
+    i++;
+   }
+    printf("foram gerador %d elementos ", i);
 
     return (0);
 }
