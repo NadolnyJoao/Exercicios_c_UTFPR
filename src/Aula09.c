d4bb7f48b41e6acfa9e66f08606fa3936a55024c
NadolnyJoao <nadolnyjoaovitor@gmail.com>
2026-09-20 00:13:41 -0300

    segundo exercicio da lista 9

--- src/Aula09.c
+++ src/Aula09.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(){
     //Lista;
+    /*
     int i, j;
     for (i = 1; i <= 10; i++){
         for(j = 1; j <= 10; j++){
@@ -8,8 +9,15 @@ int main(){
 
         }
         printf("\n");
-    }
+    }*/
 
+    int i, j, n = 4;
+    for (i = 1; i <= n; i++){
+        for(j = 1; j <= n; j++){
+        printf("A");
+        }
+        printf("\n");
+    }
 
 return 0;
 }
