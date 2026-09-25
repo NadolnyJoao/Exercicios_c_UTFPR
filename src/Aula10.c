3bb33c9bf1ae21a9e64f477dd4267c450fbb713e
nadolnyJoao <nadolnyjoaovitor@gmail.com>
2026-09-25 17:58:15 -0300

    feat: lista aula 10 exercicio 1

---
+++ src/Aula10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//Lista;
+/*
+int eh_bisexto(int ano){
+    return (ano % 400 == 0 || (ano % 4 == 0 && ano % 100 != 0));
+}
+int main(){
+    int ano_atual = 2024;
+    printf("%d", eh_bisexto(ano_atual));
+
+
+return 0;
+}*/
